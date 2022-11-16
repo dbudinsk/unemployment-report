76ec95378f867acff523684fd97befa3c19509b1
dbudinsk <dcb101@georgetown.edu>
2022-11-16 15:06:36 -0500

    saved work

---
+++ Tests/stocks_test.py
@@ -0,0 +1,28 @@
+
+
+from app.stocks import format_usd
+
+
+def test_usd_formatting(): 
+
+
+    assert format_usd(4.5) == "$4.50"
+
+    assert format_usd(4.5555555) == "$4.56"
+
+    assert format_usd(1234567890) == "$1,234,567,890.00"
+
+    #assert format_usd("OOPS") == "______"
+
+
+def test_data_fetching():
+    result = fetch_stocks_data("NFLX")
+    assert isinstance(result, DataFrame)
+
+    assert "timestamp" in result.columns
+    assert "adjusted_close" in result.columns
+    assert "high" in result.columns
+    assert "low" in result.columns
+
+
+    assert len(result) >= 100
