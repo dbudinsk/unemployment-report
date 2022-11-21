87871204fe3dc338eb732870015fd215a967c901
dbudinsk <dcb101@georgetown.edu>
2022-11-21 13:40:05 -0500

    Alpha.py

---
+++ app/alpha.py
@@ -0,0 +1,10 @@
+
+# this is the "app/alpha.py" file...
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv() # look in the ".env" file for env vars
+
+API_KEY = os.getenv("ALPHAVANTAGE_API_KEY")
+
